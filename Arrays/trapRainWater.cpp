89cbb1d9eb7f67b6ffea2bff208f7a64953c96ef
darshan51081@gmail.com <darshan51081@gmail.com>
2022-11-23 17:18:42 +0530

    arrays

---
+++ Arrays/trapRainWater.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getWater(int arr[], int n)
+{
+    int res = 0;
+    int lMax[n], rMax[n];
+    lMax[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        lMax[i] = max(arr[i], lMax[i - 1]);
+    }
+
+    rMax[n - 1] = arr[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        rMax[i] = max(arr[i], rMax[i + 1]);
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        res = res + (min(lMax[i], rMax[i]) - arr[i]);
+    }
+
+    cout << res;
+}
+
+int main()
+{
+    int arr2[] = {1, 2, 783, 4, 5};
+    int len = sizeof(arr2) / sizeof(arr2[0]);
+    getWater(arr2, len);
+    return 0;
+}
