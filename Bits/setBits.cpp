78b92d18387367e68216d1839d43940eedc13339
darshan51081@gmail.com <darshan51081@gmail.com>
2022-11-24 23:02:51 +0530

    bits

--- Bits/setBits.cpp
+++ Bits/setBits.cpp
@@ -5,9 +5,9 @@ int setBits(int n);
 
 int main()
 {
-    cout<<setBits(5);
-    
-    cout<<setBits2(5);
+    cout << setBits(5) << endl;
+
+    cout << setBits2(5) << endl;
     return 0;
 }
 
@@ -22,6 +22,7 @@ int setBits(int n)
             n = n >> 1;
         }
     }
+    return res;
 }
 
 // TC = O(n)
@@ -31,9 +32,10 @@ int setBits2(int n)
     int res = 0;
     while (n > 0)
     {
-        n = (n & (n - 1)); // Check out below comment.
+        n = n & (n - 1); // Check out below comment.
         res++;
     }
+    return res;
 }
 
 // Line 31: This line alone sets the last bit of a number from 1 to 0, reverse is not possible. It continously does until n==0;
