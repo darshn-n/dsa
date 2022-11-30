d710cd7ade7bf97a17333d608417fd7bede64165
darshan51081@gmail.com <darshan51081@gmail.com>
2022-11-30 21:20:36 +0530

    ll

---
+++ Linked List/traversal.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = null;
+    }
+};
+
+
+
+int main()
+{
+    return 0;
+}
