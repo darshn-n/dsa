f88c2f27b6543300d322522a123ee6f21dc6ac85
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-01 21:13:27 +0530

    ll

--- Linked List/traversal.cpp
+++ Linked List/traversal.cpp
@@ -1,22 +1,36 @@
+
+
 #include <bits/stdc++.h>
 using namespace std;
-
-
-
 
 struct Node
 {
     int data;
     Node *next;
-    Node(int x){
+    // Constructor:
+    Node(int x)
+    {
         data = x;
         next = null;
     }
 };
 
-
+void printList(Node *head)
+{
+    Node *curr = head; // Initialising the first element
+    while (curr != null)
+    {
+        cout << curr->data << endl;
+        curr = curr->next;
+    }
+}
 
 int main()
 {
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    printList(head);
     return 0;
 }
