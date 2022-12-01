2c5f480fef82b48ff708771ea607baf8670e7086
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-01 22:59:33 +0530

    ll

---
+++ Linked List/insertEnd.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        curr = curr->next;
+    }
+}
+
+
+Node *insertEnd(Node *head,int x){
+    // Initialise a temp node;
+    Node *temp = new Node(x);
+    if (head==NULL)
+    {
+        return temp;
+    }
+    Node *curr = head;
+    
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next=new Node(20);
+    head = insertEnd(head,30);
+    head = insertEnd(head,40);
+    head = insertEnd(head,50);
+    printList(head);
+    return 0;
+}
