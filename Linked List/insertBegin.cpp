1388e62c72d19cedcf3b393aaa1963cb96124eb0
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-01 21:50:00 +0530

    ll

---
+++ Linked List/insertBegin.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *insertBegin(Node *head, int x)
+{
+    // Create a new node temp;
+    Node *temp = new Node(x);
+
+    // Make it the first element by,
+    temp->next = head;
+
+    // Return temp variable;
+    return temp;
+}
+
+void printList(Node *head)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << endl;
+        curr = curr->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    head = insertBegin(head, 20);
+    head = insertBegin(head, 30);
+    printList(head);
+    return 0;
+}
