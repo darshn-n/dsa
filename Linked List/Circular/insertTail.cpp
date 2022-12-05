f1b3d38395660c006645781d370dcc14ab493dda
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-05 19:27:53 +0530

    cll

--- Linked List/Circular/insertTail.cpp
+++ Linked List/Circular/insertTail.cpp
@@ -12,10 +12,33 @@ struct Node
     }
 };
 
+Node *insertTailNaive(Node *head, int d)
+{
+    Node *temp = new Node(x);
+    if (head == nullptr)
+    {
+        temp->next = temp;
+        return temp;
+    }
+
+    Node *curr = head;
+    while (curr->next != head)
+    {
+        curr = curr->next;
+    }
+    curr->next = temp;
+    temp->next = head;
+    return head;
+}
+
 Node *insertTail(Node *head, int d)
 {
-    Node *temp = new Node(d);
-    
+    Node *temp = new Node(x);
+    if (head == nullptr)
+    {
+        temp->next = temp;
+        return temp;
+    }
 }
 
 int main()
