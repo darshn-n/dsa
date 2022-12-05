9a37acc3f8b1dd83288b3aee9053e092e774829c
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-05 18:45:04 +0530

    cll

--- Linked List/Circular/insertHead.cpp
+++ Linked List/Circular/insertHead.cpp
@@ -12,10 +12,48 @@ struct Node
     }
 };
 
-Node *insertHead(Node *head, int d)
+Node *insertHeadNaive(Node *head, int d)
 {
+
+    // TC : O(n);
     Node *temp = new Node(d);
-    
+    if (head == nullptr)
+    {
+        temp->next = temp;
+        return temp;
+    }
+
+    Node *curr = head;
+    while (curr->next != head)
+    {
+        curr = curr->next;
+    }
+    curr->next = temp;
+    temp->next = head;
+    return temp; // head;
+}
+
+Node *insertHeadOpt(Node *head, int x)
+{
+    Node *temp = new Node(x);
+    if (head == nullptr)
+    {
+        temp->next = temp;
+        return temp;
+    }
+
+    // 10 > 20 > 25 > 30;
+    // 10 > 15 > 20 > 25 > 30;
+
+    temp->next = head->next;
+    head->next = temp;
+
+    // Swap two data of first and second;
+    int t = head->data;
+    head->data = temp->data;
+    temp->data = t;
+
+    return head;  // head remains constant only value will be changed.
 }
 
 void printList(Node *head)
