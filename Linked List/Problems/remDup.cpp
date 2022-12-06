1f9a2194e38e3ee912c1f96730e24ada092f4f76
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-06 18:55:16 +0530

    sll

---
+++ Linked List/Problems/remDup.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    // Constructor:
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+Node *remDup(Node *head){
+    
+}
+
+
+int main()
+{
+    return 0;
+}
