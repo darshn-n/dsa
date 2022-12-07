14ea849498814b192441ff6ec3336a60dd140598
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-07 21:53:58 +0530

    sll

--- Linked List/Problems/revRecursive.cpp
+++ Linked List/Problems/revRecursive.cpp
@@ -1,10 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node *next;
+    // Constructor:
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
 
+Node *recRev(Node *head)
+{
+    // Exceptions:
+    if (head == nullptr || head->next == NULL)
+    {
+        return head;
+    }
 
-
-
+    // RecRev:
+    Node *recHead = recRev(head->next);
+    Node *recTail = head->next;
+    recTail->next = head;
+    head->next = nullptr;
+    return recHead;
+}
 
 int main()
 {
