9c826e575ffcb97a28b0a7049845a2ffc8542c33
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-07 17:05:11 +0530

    sll

--- Linked List/Problems/detectLoop.cpp
+++ Linked List/Problems/detectLoop.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
 struct Node
 {
     int data;
@@ -15,10 +13,10 @@ struct Node
     }
 };
 
-
-
-
 int main()
 {
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
     return 0;
 }
