81d42d387b20d6154ca01a0695b50502557275a0
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-09 19:36:50 +0530

    sll

---
+++ Linked List/Problems/lruCache.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+struct Node
+{
+    int data;
+    Node *next;
+    // Constructor:
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+
+
+
+int main()
+{
+    return 0;
+}
