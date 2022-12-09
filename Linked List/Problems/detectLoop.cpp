d13ea36a2c389504589fcaff78b56b0a4ef2f300
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-09 21:05:15 +0530

    sll

--- Linked List/Problems/detectLoop.cpp
+++ Linked List/Problems/detectLoop.cpp
@@ -1,127 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node
+struct Node
 {
-public:
-    int key;
-    int value;
-    Node *pre;
+    int data;
     Node *next;
-
-    Node(int k, int v)
+    bool visited;
+    // Constructor:
+    Node(int x)
     {
-        key = k;
-        value = v;
-        pre = NULL;
+        data = x;
         next = NULL;
+        visited = false;
     }
 };
 
-class LRUCache
+bool isLoop(Node *head)
 {
-public:
-    unordered_map<int, Node *> map;
-    int capacity, count;
-    Node *head, *tail;
-
-    LRUCache(int c)
+    Node *temp = new Node(1);
+    Node *curr = head;
+    while (curr->next != nullptr)
     {
-        capacity = c;
-        head = new Node(0, 0);
-        tail = new Node(0, 0);
-        head->next = tail;
-        tail->pre = head;
-        head->pre = NULL;
-        tail->next = NULL;
-        count = 0;
-    }
-
-    void deleteNode(Node *node)
-    {
-        node->pre->next = node->next;
-        node->next->pre = node->pre;
-    }
-
-    void addToHead(Node *node)
-    {
-        node->next = head->next;
-        node->next->pre = node;
-        node->pre = head;
-        head->next = node;
-    }
-
-    int get(int key)
-    {
-        if (map[key] != NULL)
+        if (curr->next == nullptr)
         {
-            Node *node = map[key];
-            int result = node->value;
-            deleteNode(node);
-            addToHead(node);
-            cout << "Got the value : " << result << " for the key: " << key << endl;
-            return result;
+            return false; // When is reaches to the last node;
         }
-        cout << "Did not get any value"
-             << " for the key: " << key << endl;
-        return -1;
-    }
+        if (curr->next == temp)
+        {
+            return true;
+        }
 
-    void set(int key, int value)
-    {
-        cout << "Going to set the (key, "
-             << "value) : (" << key << ", " << value << ")" << endl;
-        if (map[key] != NULL)
-        {
-            Node *node = map[key];
-            node->value = value;
-            deleteNode(node);
-            addToHead(node);
-        }
-        else
-        {
-            Node *node = new Node(key, value);
-            map[key] = node;
-            if (count < capacity)
-            {
-                count++;
-                addToHead(node);
-            }
-            else
-            {
-                map.erase(tail->pre->key);
-                deleteNode(tail->pre);
-                addToHead(node);
-            }
-        }
+        Node *newCurr = curr->next;
+        curr->next = temp;
+        curr = newCurr;
     }
-};
+    return false;
+}
 
 int main()
 {
-    {
-
-        LRUCache cache(2);
-
-        // it will store a key (1) with value
-        // 10 in the cache.
-        cache.set(1, 10);
-
-        // it will store a key (2) with value 20 in the cache.
-        cache.set(2, 20);
-        cout << "Value for the key: 1 is " << cache.get(1) << endl; // returns 10
-
-        // removing key 2 and store a key (3) with value 30 in the cache.
-        cache.set(3, 30);
-
-        cout << "Value for the key: 2 is " << cache.get(2) << endl; // returns -1 (not found)
-
-        // removing key 1 and store a key (4) with value 40 in the cache.
-        cache.set(4, 40);
-        cout << "Value for the key: 1 is " << cache.get(1) << endl; // returns -1 (not found)
-        cout << "Value for the key: 3 is " << cache.get(3) << endl; // returns 30
-        cout << "Value for the key: 4 is " << cache.get(4) << endl; // return 40
-
-        return 0;
-    }
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    return 0;
 }
