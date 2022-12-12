c363f9740dfe77ca56ae93f56147fe8eb2921cea
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-12 21:58:10 +0530

    stack

--- Stack/linkedListStack.cpp
+++ Stack/linkedListStack.cpp
@@ -1,11 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node *next;
+    // Constructor:
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
 
+struct MyStack
+{
+    Node *head;
+    int sz;
+    MyStack()
+    {
+        head = nullptr;
+        sz = 0;
+    }
+    void push(int x)
+    {
+        Node *temp = new Node(x);
+        temp->next = head;
+        head = temp;
+        sz++;
+    }
+    int pop()
+    {
+        if (head == nullptr)
+        {
+            return INT_MAX;
+        }
+        int res = head->data;
+        Node *temp = head;
+        head = head->next;
+        delete (temp);
+        sz--;
+        return res;
+    }
 
-
-
-
+    int size()
+    {
+        return sz;
+    }
+    bool isEmpty()
+    {
+        return head == NULL;
+    }
+    int peek()
+    {
+        if (head == NULL)
+        {
+            return INT_MAX;
+        }
+        return head->data;
+    }
+};
 
 int main()
 {
