4be5b8f8b3aecdafed514f1df19c777e3511cad5
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-18 10:53:59 +0530

    stack

--- Stack/histogram.cpp
+++ Stack/histogram.cpp
@@ -67,6 +67,33 @@ int getMaxArea2(int arr[], int n)
     return res;
 }
 
+
+
+int getMaxArea3(int arr[],int n){
+    stack <int> s;
+    int res=0;
+    int tp;
+    int curr;
+    
+    for(int i=0;i<n;i++){
+        while(s.empty()==false && arr[s.top()]>=arr[i]){
+            tp=s.top();s.pop();
+            curr=arr[tp]* (s.empty() ? i : i - s.top() - 1);
+            res=max(res,curr);
+        }
+        s.push(i);
+    }
+    while(s.empty()==false){
+        tp=s.top();s.pop();
+        curr=arr[tp]* (s.empty() ? n : n - s.top() - 1);
+        res=max(res,curr);
+    }
+    
+    return res;
+    
+}
+
+
 int main()
 {
     int arr[] = {6, 2, 5, 4, 1, 5, 6};
