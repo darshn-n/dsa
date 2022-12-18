2bd04f4f76cf163feabcad624fbcf9b5b94ed1d0
darshan51081@gmail.com <darshan51081@gmail.com>
2022-12-18 10:40:21 +0530

    stack

--- Stack/previousGre.cpp
+++ Stack/previousGre.cpp
@@ -20,10 +20,26 @@ void printPrevGreater(int arr[], int n)
     }
 }
 
+void printPrevGreater2(int arr[], int n)
+{
+    stack<int> s;
+    s.push(arr[0]);
+    for (int i = 0; i < n; i++)
+    {
+        while (s.empty() == false && s.top() <= arr[i])
+            s.pop();
+        int pg = s.empty() ? -1 : s.top();
+        cout << pg << " ";
+        s.push(arr[i]);
+    }
+}
+
 int main()
 {
     int arr[] = {20, 30, 10, 5, 15};
     int n = 5;
     printPrevGreater(arr, n);
+    printPrevGreater2(arr, n);
+
     return 0;
 }
