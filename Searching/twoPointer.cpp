842d808ddc4b74f69b01e62d698c66216fa77a80
darshan51081@gmail.com <darshan51081@gmail.com>
2023-03-04 08:25:48 +0530

    pointer

--- Searching/twoPointer.cpp
+++ Searching/twoPointer.cpp
@@ -16,10 +16,36 @@ bool isPair(int x, int arr[], int n)
     return false;
 }
 
+// Efficient approach:
+// If the sum of any end elements form less than the target, then ignore the left most element you took.
+// Else ignore right element you took.
+
+bool isPairEfficient(int x, int arr[], int n)
+{
+    int low = 0, high = n - 1;
+    while (low < high)
+    {
+        if (arr[low] + arr[high] == x)
+        {
+            return true;
+        }
+        else if (arr[low] + arr[high] < x)
+        {
+            low++;
+        }
+        else
+        {
+            high--;
+        }
+    }
+    return false;
+}
+
 int main()
 {
-    int arr1[] = {1, 2, 3, 4, 6, 5};
+    int arr1[] = {1, 2, 3, 4, 5, 6};
     int len = sizeof(arr1) / sizeof(arr1[0]);
-    cout << isPair(10, arr1, 6);
+    cout << isPair(10, arr1, 6) << endl;
+    cout << isPairEfficient(10, arr1, 6);
     return 0;
 }
