724560c7f81e8e5c6a67f0b4adacf299777cca08
darshan51081@gmail.com <darshan51081@gmail.com>
2023-07-07 18:26:50 +0530

    res

--- Graphs/dfsDis.cpp
+++ Graphs/dfsDis.cpp
@@ -6,22 +6,24 @@ void DFSRec(vector<int> adj[], int s, bool visited[])
     visited[s] = true;
     cout << s << " ";
 
-    for (int v : adj[s])
+    for (int u : adj[s])
     {
-        if (visited[v] == false)
-            DFSRec(adj, v, visited);
+        if (visited[u] == false)
+            DFSRec(adj, u, visited);
     }
 }
 
-void DFS(vector<int> adj[], int V, int s)
+void DFS(vector<int> adj[], int V)
 {
     bool visited[V];
     for (int i = 0; i < V; i++)
         visited[i] = false;
 
     for (int i = 0; i < V; i++)
-        if (visited[i] = false)
-            DFSRec(adj, s, visited);
+    {
+        if (visited[i] == false)
+            DFSRec(adj, i, visited);
+    }
 }
 
 void addEdge(vector<int> adj[], int u, int v)
@@ -36,13 +38,11 @@ int main()
     vector<int> adj[V];
     addEdge(adj, 0, 1);
     addEdge(adj, 0, 2);
-    addEdge(adj, 2, 3);
-    addEdge(adj, 1, 3);
-    addEdge(adj, 1, 4);
+    addEdge(adj, 1, 2);
     addEdge(adj, 3, 4);
 
-    cout << "Following is Depth First Traversal: " << endl;
-    DFS(adj, V, 0);
+    cout << "Following is Depth First Traversal for disconnected graphs: " << endl;
+    DFS(adj, V);
 
     return 0;
 }
