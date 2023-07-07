c36cc003413d7c73d06bb628a07dc76cb84c78d9
darshan51081@gmail.com <darshan51081@gmail.com>
2023-07-07 07:50:48 +0530

    test

---
+++ Graphs/adjList.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void addEdge(vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void printGraph(vector<int> adj[], int v)
+{
+    for (int i = 0; i < v; i++)
+    {
+        for (int x : adj[i])
+        {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int v = 4;
+    vector<int> adj[v];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 2);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 3, 1);
+    printGraph(adj, v);
+    return 0;
+}
